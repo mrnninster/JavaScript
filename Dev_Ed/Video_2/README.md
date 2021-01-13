d324121b44be8dbe9f0c700ad4069c52d170f02a
Akinsola <adefolahanakinsola@gmail.com>
2021-01-13 05:32:17 +0100

    End of Video 2

---
+++ Dev_Ed/Video_2/README.md
@@ -0,0 +1,11 @@
+## PART 2 VARIABLES AND DATATYPES
+
+This is how you make a comment "//"
+Strings, Integers, Booleans, null and undefined will be covered
+Variables are a means of storing value, think of it like a shopping basket you can create for anything
+We assign variables using the "=" sign as you will see in this script
+The old way or creating variables uses the keyword "var" as you will see and the new way use the keywords "let" and "const"
+
+- const stands for constant and means the variable content will not be changes
+- let allows you change the content of the variable
+- Both "let" and "const" would not not allows you re declare a variables, so variables can only be declared once however "var" does allow this
--- Dev_Ed/Video_2/index.js
+++ Dev_Ed/Video_2/index.js
@@ -1,43 +1,48 @@
-// This is how you make a comment
-
 // Assigning a Number
-let Game_life  = 100;
+let Game_life = 100;
 
 // Assigning a string
-let Game_User_Name = "Mrnninster"
+const Game_User_Name = "Mrnninster";
 
 // Print Status
-let status = Game_User_Name+" currently has a life of " +Game_life
+let status = Game_User_Name + " currently has a life of " + Game_life;
 console.log(status);
 
 // ---Demonstrating Some Arithmetic operation---
 
 // Taking Damage attack from minions  (Subtraction)
-Damage = 40;    //Damage Value
-Game_life = Game_life - Damage;    // Subtracting Damage amd getting new damage game life value
-status = Game_User_Name+" currently has a life of " +Game_life;
+const Damage = 40; //Damage Value
+Game_life = Game_life - Damage; // Subtracting Damage amd getting new damage game life value
+status = Game_User_Name + " currently has a life of " + Game_life;
 console.log(status);
 
 // Taking Damage Boss Attack  (Division)
-Game_life = Game_life / 2;    // Subtracting Damage amd getting new damage game life value
-status = Game_User_Name+" currently has a life of " +Game_life;
+Game_life = Game_life / 2; // Subtracting Damage amd getting new damage game life value
+status = Game_User_Name + " currently has a life of " + Game_life;
 console.log(status);
-
 
 // Receiving a health Pack (Addition)
-Heal = 20;
+const Heal = 20;
 Game_life = Game_life + Heal;
-status = Game_User_Name+" currently has a life of " +Game_life;
+status = Game_User_Name + " currently has a life of " + Game_life;
 console.log(status);
-
 
 // Team Health Buff (Multiplication)
 Game_life = Game_life * 2;
-status = Game_User_Name+" currently has a life of " +Game_life;
+status = Game_User_Name + " currently has a life of " + Game_life;
 console.log(status);
 
-
 // Boolean
-check_out = true;   // Puchase items from the game store
+let check_out = true; // Puchase items from the game store
+console.log(check_out);
 
-check_out = false;  // Maybe insufficient funds
+let no_check_out = false; // Maybe insufficient funds
+console.log(no_check_out);
+
+// Undefined
+let mrnninster;
+console.log(mrnninster);
+
+// Null
+let null_val = null;
+console.log(null_val);
